7805f3a9c443c2b558247276624b1d77a6beeaad
Kealan Crilly <crilly2k12@hotmail.co.uk>
2020-11-30 12:48:09 +0000

    Attempted the getPropertiesPriceAbove method.

--- src/main/java/com/dkit/oop/App.java
+++ src/main/java/com/dkit/oop/App.java
@@ -9,23 +9,20 @@ import java.util.ArrayList;
  * <p>
  * Your GitHub link:https://github.com/Kealan94/oop-ca2.git
  */
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         System.out.println("\n*** Property & Private Property- - App ***");
         App app = new App();
         app.start();
     }
 
-    public void start()
-    {
+    public void start() {
         System.out.println("Q1 - some simple tests");
 
         Property p1 = new Property(1, "Mary Smith", "A91AB12", 255000, 130, "mains water");
         Property p2 = new Property(2, "John Smith", "A92AB11,", 255500, 120, "electricity");
         System.out.println("Property p1 details:");
-        System.out.println(p1.getPropertyID() + " : " + p1.getOwner() + " : " + p1.getPostcode() + " : " + p1.getSellingPrice() + " : " + p1.getArea()) + " " + p1.getFacilities();
+        //System.out.println(p1.getPropertyID() + " : " + p1.getOwner() + " : " + p1.getPostcode() + " : " + p1.getSellingPrice() + " : " + p1.getArea()) + " " + p1.getFacilities();
 
 
         System.out.println("Q2 - tests");
@@ -34,15 +31,28 @@ public class App
         props.add(new Property(4, "James Murphy", "A99AB16", 250043, 130, "private access"));
         props.add(new Property(5, "Jane Malloy", "A99AB17", 27004, 144, "mains"));
         displayProperties(props);
+        getPropertiesPriceAbove(props);
     }
 
 
-public static void displayProperties( ArrayList<Property>props){
-    for (Property properties:props){
-        System.out.println(Property + "" +properties.getArea() + "" + properties.getFacilities() + " "
+
+public static void displayProperties(ArrayList<Property>props) {
+    for (Property properties : props) {
+        System.out.println(properties + "" + properties.getArea() + "" + properties.getFacilities() + " "
                 + properties.getOwner() + "" + properties.getPostcode() + "" + properties.getSellingPrice() + "" + properties.getPropertyID());
     }
+}
 
+public static void getPropertiesPriceAbove(ArrayList<Property>props){
+        int boundary = 29000;
+        for (Property properties : props){
+            if (properties.getSellingPrice()> boundary)
+            {
+                System.out.println();
+            }
+
+
+    }
         System.out.println("Q3 - tests");
 
 
