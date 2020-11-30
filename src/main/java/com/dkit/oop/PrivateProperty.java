2ada3d727a5b800f2e674efca914b06fd4de7417
Kealan Crilly <crilly2k12@hotmail.co.uk>
2020-11-30 12:53:09 +0000

    Made the PrivateProperty class inherit from property.

--- src/main/java/com/dkit/oop/PrivateProperty.java
+++ src/main/java/com/dkit/oop/PrivateProperty.java
@@ -2,9 +2,14 @@ package com.dkit.oop;
 
 import java.util.Objects;
 
-public class PrivateProperty
-{
+public class PrivateProperty extends Property {
+    private int numBeds;
+    private String propertyType;
 
 
-
+    public PrivateProperty(int propertyID, String owner, String postcode, double sellingPrice, double area, String facilities, int numBeds, String propertyType) {
+        super(propertyID, owner, postcode, sellingPrice, area, facilities);
+        this.numBeds = numBeds;
+        this.propertyType = propertyType;
+    }
 }
